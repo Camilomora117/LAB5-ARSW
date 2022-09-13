0a5938a5f78457f0bc034ea815f3513ff0e3d07c
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-09-12 19:46:43 -0500

    Primera parte completa

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -10,21 +10,24 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.gson.Gson;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.service.BlueprintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.PageRanges;
 
 /**
  *
  * @author hcadavid
  */
 @RestController
-@RequestMapping(value = "v1/blueprints")
+@RequestMapping(value = "/v1/blueprints")
 public class BlueprintAPIController {
 
     @Autowired
@@ -33,11 +36,36 @@ public class BlueprintAPIController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> getBluePrints() {
         try {
-            //obtener datos que se enviarán a través del API
-            return new ResponseEntity<>(blueprintsServices.getAllBlueprints(), HttpStatus.ACCEPTED);
+            Set<Blueprint> blueprints = blueprintsServices.getAllBlueprints();
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJson(blueprints), HttpStatus.ACCEPTED);
         } catch (BlueprintPersistenceException ex) {
             Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("Error en el metodo getAllBluePrints", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{author}")
+    public ResponseEntity<?> getAuthor(@PathVariable String author) {
+        try {
+            Set<Blueprint> blueprints = blueprintsServices.getBlueprintsByAuthor(author);
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJson(blueprints), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error, No se encontro el Author", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{author}/{bpname}")
+    public ResponseEntity<?> getBlueprintAuthor(@PathVariable String author, @PathVariable String bpname) {
+        try {
+            Blueprint blueprint = blueprintsServices.getBlueprint(author, bpname);
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJson(blueprint), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error, No se encontro el Author o el Blueprint", HttpStatus.NOT_FOUND);
         }
     }
 
